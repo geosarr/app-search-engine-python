b19dfd28c7ab3fec421c9e8e02086e303bcb44e6
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-10-02 19:31:48 +0200

    added pagination and some PEP

--- app/app.py
+++ app/app.py
@@ -1,16 +1,60 @@
 import streamlit as st
-from utils import model, clusters, design_output, empty_result
+from utils import model, design_output, empty_result, clusters, pagination, display_duration
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from constants import MAX_NUMBER_RESULTS_PER_PAGE
+from time import time
 
 
 def main():
+
     st.title('Wikipedia Abstracts Search Engine')
+
+    if "page_number" not in st.session_state:
+        st.session_state["page_number"] = 0
+    if "query" not in st.session_state:
+        st.session_state["query"] = ""
+    if "duration" not in st.session_state:
+        st.session_state["duration"] = -1
+
     query = st.text_input(label="Enter your query please : ", value="")
+    time_start = time()
+
+    if query != st.session_state.query:
+        st.session_state.query = query
+        st.session_state.page_number = 0
+        st.session_state.duration = -1
+
     if len(query.strip())==0:
         st.write(empty_result(), unsafe_allow_html=True)
     else:
         results = model.retrieval(query, clusters)
-        top_abstracts_urls = list(map(lambda x: (model.index.documents[x[0]].content, model.index.documents[x[0]].url, model.index.documents[x[0]].title) , results))
-        st.write(design_output(top_abstracts_urls, query.split()), unsafe_allow_html=True)
-
+        # st.write(f"There are {len((results))} results")
+        if st.session_state.duration == -1:
+            st.session_state.duration = time() - time_start
+        st.write(display_duration(st.session_state.duration), 
+                 unsafe_allow_html=True
+        )
+        top_abstracts_urls = list(map(lambda x: (model.index.documents[x[0]].content, 
+                                                 model.index.documents[x[0]].url, 
+                                                 model.index.documents[x[0]].title
+                                      ) , 
+                                      results
+                                     )
+        )
+        idx_start = st.session_state.page_number * MAX_NUMBER_RESULTS_PER_PAGE
+        idx_end = (1 + st.session_state.page_number) * MAX_NUMBER_RESULTS_PER_PAGE
+        results_to_show = top_abstracts_urls[idx_start : idx_end]
+        st.write(design_output(results_to_show, 
+                               query.split(), 
+                               st.session_state.page_number
+                 ), 
+                 unsafe_allow_html=True
+        )
+        
+        st.session_state.page_number = pagination(results=results, 
+                                                  page_number=st.session_state.page_number
+        )
 if __name__ == '__main__':
     main()
--- app/utils.py
+++ app/utils.py
@@ -8,24 +8,34 @@ from numpy import random as rd
 from numpy import mean, log10, Inf
 from tqdm import tqdm
 import streamlit as st
+from constants import MAX_NUMBER_RESULTS_PER_PAGE
+from indices import InvertedIndex
 
 
-def get_model(top=100):
+def get_model(top: int=100):
     data_dir=os.path.join(one_level_up, "data")
     if os.path.exists(os.path.join(data_dir,"inv.pickle")):
         print("\nLoading the index")
-        try:
-            index = read_pickle("inv", os.path.join(one_level_up, "data"))
-            print("..... OK\n")
-        except Exception as e:
-            print(f"\nFailed to load the data : {e}")
+        index = read_pickle("inv", os.path.join(one_level_up, "data"))
+        print("..... OK\n")
     else:
-        index, _ = indexing(dataset="wiki", save_load_path=data_dir, index_type="inv", nb_docs=int(1e5))
+        index, _ = indexing(dataset="wiki", 
+                            save_load_path=data_dir, 
+                            index_type="inv", 
+                            nb_docs=int(1e5)
+        )
     model=BM25(index=index, top=top)
     return model
 
-def cluster_docs(index, k=1.5, b=0.75, n_centers=500, save=True, save_path=os.path.join(one_level_up, "data")):
+def cluster_docs(index: InvertedIndex, 
+                 k: float=1.5, 
+                 b: float=0.75, 
+                 n_centers=500, 
+                 save: bool=True, 
+                 save_path: str=os.path.join(one_level_up, "data")):
+
     file_path = os.path.join(one_level_up, "data", "doc_clusters_bm25.pickle")
+
     if not os.path.exists(file_path):
         K=len(index.documents)
         l_avg=mean([len(index.raw_freq[ID]) for ID in index.documents])
@@ -40,9 +50,12 @@ def cluster_docs(index, k=1.5, b=0.75, n_centers=500, save=True, save_path=os.pa
             for c in centers:
                 overlap_terms = doc_terms.intersection(set(centers_terms[c]))
                 l_doc=len(index.raw_freq[c])
-                score = sum({(index.raw_freq[c][term]*(k+1)/(k*(1-b)+index.raw_freq[c][term]+k*l_doc*b/l_avg)) * \
-                            log10(0.5*K/len(index.index[term]))
-                            for term in overlap_terms})
+                score = sum({(
+                        index.raw_freq[c][term]*(k+1)/(k*(1-b) + 
+                        index.raw_freq[c][term]+k*l_doc*b/l_avg)) * \
+                        log10(0.5*K/len(index.index[term]))
+                    for term in overlap_terms
+                })
                 if score>=doc_score:
                     clust=c
                     doc_score=score
@@ -59,15 +72,17 @@ def cluster_docs(index, k=1.5, b=0.75, n_centers=500, save=True, save_path=os.pa
     return clusters
 
                 
-
-	
-def design_output(text_results, words_to_highlight):
+def design_output(text_results: list, 
+                  words_to_highlight: list, 
+                  page_number: int) -> str:
+                  
     if len(text_results)==0:
         return '''<div>No result found</div>'''
     words_to_highlight=list(map(lambda x: x.lower().strip(), words_to_highlight))
     template= '''<div> <br>'''
     for pos, (text, url, title) in enumerate(text_results):
-        template+=f'''<a href={url}>{url if len(url)<=100 else url[:100]+ '...'}</a><br>'''
+        template+=f'''<a href={url}>{MAX_NUMBER_RESULTS_PER_PAGE*page_number+pos+1}.'''+\
+                  f'''{url if len(url)<=100 else url[:100]+ '...'}</a><br>'''
         for w in text.split():
             if w.lower().strip() in words_to_highlight:
                 template+=f''' <b>{w}</b>'''
@@ -77,9 +92,41 @@ def design_output(text_results, words_to_highlight):
     template+=''' </div>'''
     return template
 
+
 def empty_result():
     return '''<div>'''
 
+
+def display_duration(duration: float) -> str:
+    return f"<div>The search took {round(duration,3)} second(s)</div>"
+
+def pagination(page_number: int, results: list) -> int:
+    if len(results) == 0:
+        # no pagination
+        return page_number
+
+    # paginate otherwise        
+    final_page = len(results) // MAX_NUMBER_RESULTS_PER_PAGE
+
+    prev, _, __, ___, center , ____, ____, ______, next = st.columns(9)
+
+
+    with center:
+        st.write(f"Page {page_number+1}")
+
+    if next.button("Next"):
+        if page_number + 1 > final_page:
+            page_number = final_page
+        else:
+            page_number += 1
+
+    if prev.button("Previous"):
+        if page_number - 1 < 0:
+            page_number = 0
+        else:
+            page_number -= 1
+    
+    return page_number
 
 
 with st.sidebar:
